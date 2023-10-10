53bcc2ed2135d23af43e25266ecf0c4c0a22bf08
Ítalo Ramalho <matheus.ramalho1354@gmail.com>
2023-10-10 00:37:09 -0300

    implementação selenium

--- exercicio-5/gui-testing-cypress-selenium-main/selenium/e2e/shipments.js
+++ exercicio-5/gui-testing-cypress-selenium-main/selenium/e2e/shipments.js
@@ -23,7 +23,7 @@ describe('shipments', () => {
   });
 
   // Remove .only and implement others test cases!
-  it.only('ship a ready shipment', async () => {
+  it('ship a ready shipment', async () => {
     // Click in shipments in side menu
     await driver.findElement(By.linkText('Shipments')).click();
 
@@ -39,16 +39,91 @@ describe('shipments', () => {
     await buttons[0].click();
 
     // Assert that shipment has been completed
-    const bodyText = await driver.findElement(By.tagName('body')).getText();
+    const bodyText = await driver.findElement(By.css('body')).getText();
     assert(bodyText.includes('Shipment has been successfully shipped.'));
   });
 
-  it('test case 2', async () => {
-    // Implement your test case 2 code here
+  it('shipping method is UPS', async () => {
+    // Click in shipments in side menu
+    await driver.findElement(By.linkText('Shipments')).click();
+    // Select the state to search for new shipments
+    const dropdown = await driver.findElement(By.id('criteria_method'));
+    await dropdown.findElement(By.xpath("//option[. = 'UPS']")).click();
+    // Click in filter blue button
+    await driver.findElement(By.css('*[class^="ui blue labeled icon button"]')).click();
+
+    // Assert that shipment has been completed
+    const table = await driver.findElement(By.css('*[class^="ui sortable stackable very basic celled table"]')).get();
+    assert(table.includes('19-09-2023 18:22:35'));
+    assert(table.includes('07-12-2022 14:21:32'));
+    assert(table.includes('28-11-2022 07:40:08'));
+
   });
 
-  it('test case 3', async () => {
-    // Implement your test case 3 code here
+  it('shipping method is DHL', async () => {
+    // Click in shipments in side menu
+    await driver.findElement(By.linkText('Shipments')).click();
+    // Select the state to search for new shipments
+    const dropdown = await driver.findElement(By.id('criteria_method'));
+    await dropdown.findElement(By.xpath("//option[. = 'DHL Express']")).click();
+    // Click in filter blue button
+    await driver.findElement(By.css('*[class^="ui blue labeled icon button"]')).click();
+
+    // Assert that shipment has been completed
+    const table = await driver.findElement(By.css('*[class^="ui sortable stackable very basic celled table"]')).get();
+    assert(table.includes('19-09-2023 01:31:33'));
+    assert(table.includes('20-07-2023 17:33:47'));
+    assert(table.includes('20-07-2023 06:27:33'));
+    assert(table.includes('26-04-2023 07:37:47'));
+    assert(table.includes('11-03-2023 00:10:02'));
+    assert(table.includes('31-03-2023 15:52:00'));
+    assert(table.includes('22-11-2022 20:56:00'));
+    assert(table.includes('02-11-2022 03:17:13'));
+
+  });
+
+  it('shipping method is FedEx', async () => {
+    // Click in shipments in side menu
+    await driver.findElement(By.linkText('Shipments')).click();
+    // Select the state to search for new shipments
+    const dropdown = await driver.findElement(By.id('criteria_method'));
+    await dropdown.findElement(By.xpath("//option[. = 'FedEx']")).click();
+    // Click in filter blue button
+    await driver.findElement(By.css('*[class^="ui blue labeled icon button"]')).click();
+
+    // Assert that shipment has been completed
+    const table = await driver.findElement(By.css('*[class^="ui sortable stackable very basic celled table"]')).get();
+    assert(table.includes('15-05-2023 06:27:47'));
+    assert(table.includes('07-05-2023 05:36:38'));
+    assert(table.includes('08-04-2023 05:41:40'));
+    assert(table.includes('30-03-2023 15:53:22'));
+    assert(table.includes('03-03-2023 17:08:38'));
+    assert(table.includes('17-01-2023 10:54:17'));
+    assert(table.includes('02-01-2023 19:26:59'));
+
+  });
+
+  it('clear filters', async () => {
+    await driver.findElement(By.linkText('Shipments')).click();
+    const clear_button = await driver.findElement(By.css('*[class^="ui labeled icon button"]'));
+    await clear_button.click();
+    const filters = await driver.findElement(By.css('.ui > .sylius-filters > .sylius-filters__field > .field'));
+    assert(filters.findElement(By.id('criteria_state')).includes('All'));
+    assert(filters.findElement(By.id('criteria_channel')).includes('All'));
+    assert(filters.findElement(By.id('criteria_method')).includes('All'));
+  });
+
+  it('on page one', async () => {
+    await driver.findElement(By.linkText('Shipments')).click();
+    const pagination = await driver.findElement(By.css('.ui > .sylius-grid-nav > .sylius-grid-nav__pagination > .pagination menu'));
+    assert(pagination.findElement(By.id('disabled item')).getText().includes('Previous'));
+    assert(pagination.findElement(By.id('active item')).getText().includes('1'));
+  });
+
+  it('on last page', async () => {
+    await driver.findElement(By.linkText('Shipments')).click();
+    const pagination = await driver.findElement(By.css('.ui > .sylius-grid-nav > .sylius-grid-nav__pagination > .pagination menu'));
+    assert(pagination.findElement(By.id('disabled item')).getText().includes('Next'));
   });
 
   // Implement the remaining test cases in a similar manner
